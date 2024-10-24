0cb443a7bc2a12b120e6d031c5bec6d953699adc
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-10-25 02:52:40 +0600

    MovieList

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Header from "./components/Header";
+import MovieList from "./components/MovieList";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <Header />
-      <Sidebar/>
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
+        </div>
+      </main>
     </>
   );
 }
